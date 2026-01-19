cbd587e2c03e3f8646afa41a213665ab9276dc40
Veronica Martinez Cadavid <veromar.c96@gmail.com>
2026-01-19 10:28:31 -0500

    refactor: functionality implementation and changes in the UI

--- js/login.js
+++ js/login.js
@@ -1,55 +1,29 @@
-// const btnLogin = document.getElementById('btn-login');
+const btnLogin = document.getElementById('btn-login');
 
-// function userLogin(){
-//     const user = JSON.parse(localStorage.getItem('user'))
-//     const email = document.getElementById('loginEmail');
-//     const passw = document.getElementById('loginPassword');
-
-//     console.log(user)
-//     console.log(email.value)
-//     console.log(passw.value)
-
-//     if (user.email != email.value || user.password != passw.value) {
-//         console.log('entro error')
-//         const errorMsg = document.getElementById('errorMessage');
-//         errorMsg.textContent = 'error';
-//     }else {
-//         console.log('entro bien')
-//         sessionStorage.setItem('loggedUser', email.value);
-//         window.location.href = 'profiles.html'
-//     }
-// }
-
-// btnLogin.addEventListener('click', (e) => {
-//     e.preventDefault()
-//     userLogin()
-//     }
-// )
-
-document.getElementById('loginForm').addEventListener('submit', loginUser);
-
-function loginUser(e) {
-    e.preventDefault();
-
-    const email = document.getElementById('loginEmail').value.trim();
-    const password = document.getElementById('loginPassword').value.trim();
+function userLogin(){
+    const loginEmail = document.getElementById('loginEmail').value;
+    const loginPassword = document.getElementById('loginPassword').value;
+    const user = JSON.parse(localStorage.getItem('user')) || [];
+    const currentUser = user.find(u => u.email === loginEmail && u.password === loginPassword);
     const error = document.getElementById('errorMessage');
 
-    if (!email || !password) {
+    if (!loginEmail || !loginPassword) {
         error.textContent = 'All fields are required';
+        return
+    } 
+
+    if (!currentUser) {
+        error.innerHTML = '<p>Invalid email or password. <a href="register.html" style="color:white;text-decoration: underline"> Sign-in</a></p>';
         return;
+    } 
+
+    if (currentUser.email === loginEmail || currentUser.password === loginPassword) {
+        sessionStorage.setItem('loggedUser', email);
+        window.location.href = 'profiles.html';
     }
-
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-
-    const user = users.find(u => u.email === email && u.password === password);
-
-    if (!user) {
-        error.textContent = 'Invalid email or password';
-        return;
-    }
-
-    sessionStorage.setItem('loggedUser', email);
-
-    window.location.href = 'profiles.html';
 }
+
+btnLogin.addEventListener('click', (e) =>{
+    e.preventDefault();
+    userLogin()
+})
